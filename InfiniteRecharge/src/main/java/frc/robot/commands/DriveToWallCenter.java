10657a7e566ce48807d9f8c86fbad9ff232a56f2
Centreville Robotics <programming@centrevillerobotics.net>
2020-03-02 17:56:16 -0500

    Auton
    Auton Park/Shoot

---
+++ InfiniteRecharge/src/main/java/frc/robot/commands/DriveToWallCenter.java
@@ -0,0 +1,26 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.RobotContainer;
+import frc.robot.subsystems.DriveSubsystem;
+
+// NOTE:  Consider using this command inline, rather than writing a subclass.  For more
+// information, see:
+// https://docs.wpilib.org/en/latest/docs/software/commandbased/convenience-features.html
+public class DriveToWallCenter extends SequentialCommandGroup {
+  /**
+   * Creates a new DriveToWall.
+   */
+  public DriveToWallCenter(RobotContainer m_robotContainer) {
+    // Add your commands in the super() call, e.g.
+    // super(new FooCommand(), new BarCommand());
+    super(new DriveStraightCommand(m_robotContainer.getDriveSubsystem(), 1, 80));
+  }
+}
